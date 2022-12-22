ab858812256fbc4c540b22a65ec5b26ccffaefcb
RowDog <mrdiamonddirt@gmail.com>
2022-12-22 19:44:29 +0000

    added all functions to the game scene

--- src/main.js
+++ src/main.js
@@ -10,118 +10,123 @@ let wheelRotation = 0;
 let engineOn = true;
 let carAngle = 0;
 
-const car = add([
-    pos(120, 80),
-    area(),
-    body({
-        friction: 0.1,
-        mass: 1,
-        shape: "square",
-        inertia: 0.1,
-        speed: 0,
-    }),
-    solid(),
-    scale(0.1),
-    rotate(carAngle),
-    origin("center"),
-    sprite("car"),
-    // components
-    {
-        dead: false,
-        speed: 0,
-    },
-]);
+scene("game", () => {
+    const wheel = add([
+        pos(450, 80),
+        scale(0.1),
+        rotate(wheelRotation),
+        origin("center"),
+        sprite("wheel"),
+        z(1),
+    ]);
 
-const speedometer = add([
-    pos(850, 80),
-    origin("center"),
-    text(car.speed + " mph", {
-        size: 32,
-        color: rgb(255, 255, 255),
-    }),
-]);
+    const car = add([
+        pos(120, 80),
+        area(),
+        body({
+            friction: 0.1,
+            mass: 1,
+            shape: "square",
+            inertia: 0.1,
+            speed: 0,
+        }),
+        solid(),
+        scale(0.1),
+        rotate(carAngle),
+        origin("center"),
+        sprite("car"),
+        // components
+        {
+            dead: false,
+            speed: 0,
+        },
+    ]);
 
-const wheel = add([
-    pos(450, 80),
-    scale(0.1),
-    rotate(wheelRotation),
-    origin("center"),
-    sprite("wheel"),
-    z(1),
-]);
+    const speedometer = add([
+        pos(850, 80),
+        origin("center"),
+        text(car.speed + " mph", {
+            size: 32,
+            color: rgb(255, 255, 255),
+        }),
+    ]);
 
-onClick(() => {
-    console.log("clicked");
+    onClick(() => {
+        console.log("clicked");
+    });
+
+    onKeyDown("up", () => {
+        console.log("up");
+        // add a force to the car
+        if (engineOn) {
+            car.speed += 2;
+            speedometer.text = car.speed;
+        }
+    });
+
+    onKeyDown("down", () => {
+        console.log("down");
+        // stop the car
+        if (car.speed > 0) {
+            car.speed -= 1;
+            speedometer.text = car.speed;
+        }
+    });
+
+    onKeyDown("left", () => {
+        if (wheelRotation >= -90) {
+            console.log("left");
+            wheelRotation -= 1;
+        }
+        console.log(wheelRotation);
+        wheel.angle = wheelRotation;
+    });
+
+    onKeyDown("right", () => {
+        if (wheelRotation <= 90) {
+            console.log("right");
+            wheelRotation += 1;
+        }
+        console.log(wheelRotation);
+        wheel.angle = wheelRotation;
+    });
+    onUpdate(() => {
+        currentCarRotation = carAngle;
+        if (car.speed > 0) {
+            car.speed -= 1;
+            speedometer.text = car.speed;
+            car.pos.x += car.speed;
+        }
+    });
+
+    addLevel(
+        [
+            "==============================",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "=                            =",
+            "==============================",
+        ],
+        {
+            // define the size of each block
+            width: 50,
+            height: 50,
+            // define what each symbol means, by a function returning a component list (what will be passed to add())
+            "=": () => [sprite("wall"), area(), scale(0.1), solid()],
+            "^": () => [sprite("objective"), area(), pos(0, 0)],
+            // C: () => [sprite("car"), area(), scale(0.1), pos(0, 0)],
+            // "^": () => [sprite("enemy"), area(), "danger"],
+        }
+    );
 });
 
-onKeyDown("up", () => {
-    console.log("up");
-    // add a force to the car
-    if (engineOn) {
-        car.speed += 2;
-        speedometer.text = car.speed;
-    }
-});
-
-onKeyDown("down", () => {
-    console.log("down");
-    // stop the car
-    if (car.speed > 0) {
-        car.speed -= 1;
-        speedometer.text = car.speed;
-    }
-});
-
-onKeyDown("left", () => {
-    if (wheelRotation >= -90) {
-        console.log("left");
-        wheelRotation -= 1;
-    }
-    console.log(wheelRotation);
-    wheel.angle = wheelRotation;
-});
-
-onKeyDown("right", () => {
-    if (wheelRotation <= 90) {
-        console.log("right");
-        wheelRotation += 1;
-    }
-    console.log(wheelRotation);
-    wheel.angle = wheelRotation;
-});
-onUpdate(() => {
-    currentCarRotation = carAngle;
-    if (car.speed > 0) {
-        car.speed -= 1;
-        speedometer.text = car.speed;
-        car.pos.x += car.speed;
-    }
-});
-
-addLevel(
-    [
-        "==============================",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "=                            =",
-        "==============================",
-    ],
-    {
-        // define the size of each block
-        width: 50,
-        height: 50,
-        // define what each symbol means, by a function returning a component list (what will be passed to add())
-        "=": () => [sprite("wall"), area(), scale(0.1), solid()],
-        "^": () => [sprite("objective"), area(), pos(0, 0)],
-        // "^": () => [sprite("enemy"), area(), "danger"],
-    }
-);
+go("game");
